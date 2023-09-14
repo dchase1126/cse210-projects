04216507f18d5776017fdabe4dfe74c84cf85371
dchase1126 <cha20068@byui.edu>
2023-09-13 18:18:15 -0600

    Core requirements 1 & 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,42 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string percentFromUser = Console.ReadLine();
+        int x = int.Parse(percentFromUser);
+         
+        if (x >= 90)
+        {
+            Console.WriteLine("A");   
+        }
+        else if (x >= 80)
+        {
+            Console.WriteLine("B");  
+        }
+        else if (x >= 70)
+        {
+            Console.WriteLine("C");      
+        }
+        else if (x >= 60)
+        {
+            Console.WriteLine("D");           
+        }
+        else
+        {
+            Console.WriteLine("F");
+        }
+
+        if (x >= 70)
+        {
+            Console.Write("Congratulations you passed!");
+        }
+        else if (x <= 69)
+        {
+            Console.Write("You can do this! Keep trying!");
+        }
     }
 }
