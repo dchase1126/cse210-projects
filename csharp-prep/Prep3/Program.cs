946c3de05b46a0b9d6c6fcf6f9cfd5577daf6d60
dchase1126 <cha20068@byui.edu>
2023-09-19 15:01:51 -0600

    core requirements-1 if else, 2 do while, 3 Random

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -2,8 +2,30 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main(string[] args) // Functions Methods Behaviours
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random rnd = new Random();
+        int number = rnd.Next(1, 100);
+        int guess;
+
+        do
+        {
+            Console.Write("What is your guess between 1-100? ");
+            guess = int.Parse(Console.ReadLine());
+
+
+            if (guess > number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < number)
+            {
+                Console.WriteLine("Higher");
+            }
+        }
+        while (guess != number);
+
+        Console.WriteLine("You guessed it!");
+
     }
 }
