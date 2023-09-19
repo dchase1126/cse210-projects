b382c8ceb9e6cb14ba10927dc867b8020b0ec775
dchase1126 <cha20068@byui.edu>
2023-09-19 16:31:35 -0600

    Started prep4 code

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -5,5 +5,15 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep4 World!");
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished. ");
+
+        Console.WriteLine("Enter a number: ");
+        
+
+
+        Console.WriteLine("The Sum is: ");
+        Console.WriteLine("The average is: ");
+        Console.WriteLine("The Largest number is: ");
     }
 }
