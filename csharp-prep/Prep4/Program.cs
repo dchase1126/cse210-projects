dadc70957025aa59de5b9e7a5308125bafc33fc8
dchase1126 <cha20068@byui.edu>
2023-09-20 16:31:47 -0600

    lists, if, while, foreach loops

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,19 +1,40 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+
+        List<double> numbers = new List<double>(); // [2, 5, 3, 4]
+        double number;
+        double sum = 0;
+        double average;
+        double largest = 0;
 
         Console.WriteLine("Enter a list of numbers, type 0 when finished. ");
 
-        Console.WriteLine("Enter a number: ");
-        
+        while (true)
+        {
+            Console.Write("Enter a number: ");
+            number = double.Parse(Console.ReadLine());
+            if (number == 0)
+                break;
+            numbers.Add(number);
+        }
 
+        foreach (double n in numbers)
+        {
+            sum += n; // sum = sum + n
+            if (n > largest)
+                largest = n;
+        }
 
-        Console.WriteLine("The Sum is: ");
-        Console.WriteLine("The average is: ");
-        Console.WriteLine("The Largest number is: ");
+        average = sum / numbers.Count;
+
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {largest}");
+
     }
 }
