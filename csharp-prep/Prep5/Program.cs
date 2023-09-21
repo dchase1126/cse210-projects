2fe16c65d9a283528e0896e33b4b4794fce65c3d
dchase1126 <cha20068@byui.edu>
2023-09-21 14:29:58 -0600

    start of Prep5 code

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        string userName = PromptUserName;
+        double userNumber = PromptUserNumber;
+
+        // static void DisplayWelcome()
+        Console.WriteLine("Welcome to the Program!");
+
+
+        //static void PromptUserName(string userName)
+        {
+            Console.WriteLine("Please enter your name: ");
+        }
+
+        // static void PromptUserNumber(int userNumber)
+        {
+            Console.WriteLine("Please enter your favorite number: ");
+        }
+
+        //SquareNumber
+        Console.WriteLine($"{userName} the square of your number is: {userNumber}");
+
+        //DisplayResult
+
+
     }
 }
