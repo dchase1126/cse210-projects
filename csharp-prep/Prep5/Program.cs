4135d73f8894214e4604e8cfcf260d79f440e2e8
dchase1126 <cha20068@byui.edu>
2023-09-21 22:39:07 -0600

    fixed the square number section

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,43 +1,57 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
+
+    // Declare Variables
     {
-        string userName = PromptUserName();
-        Double userNumber = PromptUserNumber();
         DisplayWelcomeMessage();
+        string userName = PromptUserName();
+        int userNumber = PromptUserNumber();
+        int squaredNumber = SquareNumber(userNumber);
 
-        //Display Welcome Message
-        static void DisplayWelcomeMessage()
-        {
-            Console.WriteLine("Welcome to the Program!");
-        }
-
-        //Prompt User for their Name
-        static string PromptUserName()
-        {
-            Console.WriteLine("Please enter your name: ");
-            string userName = Console.ReadLine();
-
-            return userName;
-        }
-
-        //Prompt User for a Number
-        static Double PromptUserNumber()
-        {
-            Double number;
-            Console.WriteLine("Please enter your favorite number: ");
-            number = Double.Parse(Console.ReadLine());
-
-            return number;
-        }
-
-        //Square Number
-           
-
-        //DisplayResult
-            Console.WriteLine($"{userName} the square of your number is: {userNumber}");
-
+        DisplayResult(userName, squaredNumber);
     }
+
+    //Display Welcome Message
+    static void DisplayWelcomeMessage()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    //Prompt User for their Name
+    static string PromptUserName()
+    {
+        Console.WriteLine("Please enter your name: ");
+        string userName = Console.ReadLine();
+
+        return userName;
+    }
+
+    //Prompt User for a Number
+    static int PromptUserNumber()
+    {
+        int number;
+        Console.WriteLine("Please enter your favorite number: ");
+        number = int.Parse(Console.ReadLine());
+
+        return number;
+    }
+
+    //Square Number 
+    static int SquareNumber(int number)    
+    {
+        int square = number * number; 
+        return square;
+     
+    }
+
+    //Display Results
+    static void DisplayResult(string userName, int square)
+    {
+        Console.WriteLine($"{userName} the square of your number is: {square}");
+    }
+ 
 }
