2f28092e76ef46e17068a1fba2bf51f369bb7380
dchase1126 <cha20068@byui.edu>
2023-10-27 22:59:43 -0600

    added menu choices

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -4,8 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, Welcome to your Mindfulness Activtities!");
 
-         
+        Activity activity = new Activity();
+
+        {
+            // Display the menu options
+            Console.WriteLine("\nHello, Welcome to your Mindfulness Activtities!");
+            Console.WriteLine("What activity would you like to do?\n");
+            Console.WriteLine("1. Breathing Activity");
+            Console.WriteLine("2. ReflectionActivity");
+            Console.WriteLine("3. Listing Activity");
+            Console.WriteLine("4. Run & Walk Activity");
+            Console.WriteLine("5. Quit");
+
+        }
     }
 }
